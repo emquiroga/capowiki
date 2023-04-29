66f84426b86fecf8f6b3a037f79959fd77fc5dd4
Emiliano Quiroga <emiliano.quiroga093@gmail.com>
2023-04-29 17:52:40 -0300

    selected proper topic

--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -13,16 +13,28 @@ const MOCK_DB: SavedText[] = [
       text: [`Hail! I am Rigurt the Brash. I lead our Glorious Expedition for Nord Cultural Exchange to the Dark Elves.
       Excuse please, Dunmer. The proper term for puny ones is Dunmer. Small, I mean. Not puny`, `The Skald-King is in Solitude? Shor's bones, no one tells Rigurt anything!
       Well. Rigurt has neveras failed a mission! He will do his best to see this one through, for the good of his people!`]
-  }
+  },
+  {
+    textTopic: "Razum Dar",
+    text: [`The captain happened. Fasion and Raz worked together, as agents of the Queen. Fasion was not willing to give up secrets. And so now Fasion is dead`, `Help the locals. They'll need all the help they can get. Raz hopes he can find some intel here, perhaps unearth some new leads.
+    This one has sent agents to every town in Auridon. If there are more like this Valano out there, we shall find them.`]
+}
 ]
 
 function Next() {
   const [value, setValue] = useState<string>("")
   const [openPopper, setOpenPopper] = useState<boolean>(false);
+
+  const [selectedTopic, setSelectedTopic] = useState<string>("");
+
+  const handleMouseIn = useCallback((topic: string) => {
+    setOpenPopper(true);
+    setSelectedTopic(topic);
+  }, [])
   
   const handleValue = useCallback((event) => setValue(event.target.value), [value])
 
-  const topics = useMemo(() => findTopics(MOCK_DB, value), [value])
+  const topics: string[] = useMemo(() => findTopics(MOCK_DB, value), [value])
 
   const displayTopic = useCallback((topic: string, DB: SavedText[]) => {
     const item = DB.find((item) => item.textTopic == topic)
@@ -55,10 +67,10 @@ function Next() {
           topics.length > 0 && topics.map((topic, index) => {
             return (
               <>
-                <p onMouseLeave={() => setOpenPopper(false)} onMouseOver={() => setOpenPopper(true)} key={index}>{topic}</p>
+                <p onMouseLeave={() => setOpenPopper(false)} onMouseOver={() => handleMouseIn(topic)} key={index}>{topic}</p>
                 <Popper open={openPopper}>
                   {
-                    displayTopic(topic, MOCK_DB)
+                    displayTopic(selectedTopic, MOCK_DB)
                   }
                 </Popper>
               </>
